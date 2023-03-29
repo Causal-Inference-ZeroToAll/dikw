a9fc50d11ed5371c62f21eb24187c5127f29da70
1587causalai <gongheyang03@kuaishou.com>
2023-03-29 17:48:40 +0800

    add simulate iv base data

--- dikw/dataset/__init__.py
+++ dikw/dataset/__init__.py
@@ -1,9 +1,11 @@
+# TODO: 生成个性化激励模拟数据的工具包 https://github.com/1587causalai/causalml-data
 from .regression import synthetic_data
 from .regression import simulate_nuisance_and_easy_treatment
 from .regression import simulate_randomized_trial
 from .regression import simulate_easy_propensity_difficult_baseline
 from .regression import simulate_unrelated_treatment_control
 from .regression import simulate_hidden_confounder
+from .regression import simulate_base_iv_data
 from .classification import make_uplift_classification
 
 from .synthetic import get_synthetic_preds, get_synthetic_preds_holdout
